ddfc211a8c8ca871d667ae557bb3890ce61aaded
Christoffer Dall <christoffer.dall@arm.com>
2019-01-04 21:09:05 +0100

    KVM: arm/arm64: nv: Factor out stage 2 page table data from struct kvm
    As we are about to reuse our stage 2 page table manipulation code for
    shadow stage 2 page tables in the context of nested virtualization, we
    are going to manage multiple stage 2 page tables for a single VM.
    
    This requires some pretty invasive changes to our data structures,
    which moves the vmid and pgd pointers into a separate structure and
    change pretty much all of our mmu code to operate on this structure
    instead.
    
    The new structre is called struct kvm_s2_mmu.
    
    There is no intended functional change by this patch alone.
    
    [Designed data structure layout in collaboration]
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@arm.com>

--- arch/arm64/kvm/hyp/switch.c
+++ arch/arm64/kvm/hyp/switch.c
@@ -232,9 +232,9 @@ void deactivate_traps_vhe_put(void)
 	__deactivate_traps_common();
 }
 
-static void __hyp_text __activate_vm(struct kvm *kvm)
+static void __hyp_text __activate_vm(struct kvm_s2_mmu *mmu)
 {
-	__load_guest_stage2(kvm);
+	__load_guest_stage2(mmu);
 }
 
 static void __hyp_text __deactivate_vm(struct kvm_vcpu *vcpu)
@@ -504,7 +504,7 @@ int kvm_vcpu_run_vhe(struct kvm_vcpu *vcpu)
 	sysreg_save_host_state_vhe(host_ctxt);
 
 	__activate_traps(vcpu);
-	__activate_vm(vcpu->kvm);
+	__activate_vm(vcpu->arch.hw_mmu);
 
 	sysreg_restore_guest_state_vhe(guest_ctxt);
 	__debug_switch_to_guest(vcpu);
@@ -550,7 +550,7 @@ int __hyp_text __kvm_vcpu_run_nvhe(struct kvm_vcpu *vcpu)
 	__sysreg_save_state_nvhe(host_ctxt);
 
 	__activate_traps(vcpu);
-	__activate_vm(kern_hyp_va(vcpu->kvm));
+	__activate_vm(kern_hyp_va(vcpu->arch.hw_mmu));
 
 	__hyp_vgic_restore_state(vcpu);
 	__timer_enable_traps(vcpu);
