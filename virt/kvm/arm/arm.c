d9adf257e12e66ab30056a8804b70a24ce0dded3
Marc Zyngier <marc.zyngier@arm.com>
2019-01-07 09:31:16 +0000

    fixup! KVM: arm/arm64: nv: Support multiple nested stage 2 mmu structures

--- virt/kvm/arm/arm.c
+++ virt/kvm/arm/arm.c
@@ -349,7 +349,8 @@ void kvm_arch_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 {
 	int *last_ran;
 
-	kvm_vcpu_load_hw_mmu(vcpu);
+	if (nested_virt_in_use(vcpu))
+		kvm_vcpu_load_hw_mmu(vcpu);
 
 	last_ran = this_cpu_ptr(vcpu->arch.hw_mmu->last_vcpu_ran);
 
@@ -383,7 +384,8 @@ void kvm_arch_vcpu_put(struct kvm_vcpu *vcpu)
 	kvm_vcpu_put_sysregs(vcpu);
 	kvm_timer_vcpu_put(vcpu);
 	kvm_vgic_put(vcpu);
-	kvm_vcpu_put_hw_mmu(vcpu);
+	if (nested_virt_in_use(vcpu))
+		kvm_vcpu_put_hw_mmu(vcpu);
 
 	vcpu->cpu = -1;
 
